77a8f05d342a07c2ee5b4e15108fe6a125ab0440
@be <abeahmed2@gmail.com>
2016-11-30 02:01:12 -0500

    Added vali_fnd_move function test.

--- proj10_test.py
+++ proj10_test.py
@@ -29,17 +29,28 @@ class TestProj10(unittest.TestCase):
         """
         Initialize a game.
         """
+        # grab a game
         self._fnd, self._tab, self._stock, self._waste = proj10.setup_game()
+        # grab a few cards
+        self._heart_two_card = cards.Card(rank=2, suit=3)
+        self._heart_three_card = cards.Card(rank=3, suit=3)
+        self._club_two_card = cards.Card(rank=2, suit=1)
+        self._club_three_card = cards.Card(rank=3, suit=1)
+        # face down card
+        self._face_down_card = cards.Card(rank=1, suit=3)
+        if self._face_down_card.is_face_up():
+            self._face_down_card.flip_card()
+
 
         # with open('enbody_game_elements.pckl', 'wb') as outfile:
             # (fnd, tab, stock, waste) = proj10.setup_game()
             # enbody_game_elements = {'tab': tab, 'fnd':fnd, 'stock': stock, 'waste': waste}
             # pickle.dump(enbody_game_elements, outfile)
 
-        enbody_game_elements = {} # temp
+        self._enbody_game_elements_dict = {} # temp
         try:
             with open('enbody_game_elements.pckl', 'rb') as infile:
-                enbody_game_elements = pickle.load(infile)
+                self._enbody_game_elements_dict = pickle.load(infile)
         except FileNotFoundError:
             raise FileNotFoundError('enbody_game_elements.pckl is required for this test.')
 
@@ -61,20 +72,52 @@ class TestProj10(unittest.TestCase):
         # Check all of the elements to ensure the game was setup correctly.
         #
         # foundation
-        self.assertListEqual(self._fnd, enbody_game_elements['fnd'],
+        self.assertListEqual(self._fnd, self._enbody_game_elements_dict['fnd'],
                 msg='Foundation should be a list containing 4 empty lists initially.')
         # tableau
-        self.assertListEqual(self._tab, enbody_game_elements['tab'],
+        self.assertListEqual(self._tab, self._enbody_game_elements_dict['tab'],
                 msg='Tableau was not distributed in the correct manner. Check project PDF.\
                         Are you sure your project is using cards1.py?')
         # stock
-        self.assertListEqual(self._stock, enbody_game_elements['stock'],
+        self.assertListEqual(self._stock, self._enbody_game_elements_dict['stock'],
                 msg='Stock was not setup in the correct manner. Check the project PDF.\
                         Are you sure your project is using cards1.py?')
         # waste
-        self.assertListEqual(self._waste, enbody_game_elements['waste'],
+        self.assertListEqual(self._waste, self._enbody_game_elements_dict['waste'],
                 msg='Waste was not setup in the correct manner. Check t eh project PDF.\
                         Are you sure your project is using cards1.py?')
+
+    def testValidFoundationMoveFunction(self):
+        """
+        valid_fnd_move(src_card : cards.Card, dest_Card : cards.Card)
+        Returns:
+            True - if move is valie bsed on requirenments below.
+
+        Moving one card from foundation A(src_card) to foundation B(dest_card) requires that:
+            - src_card and dest_card be face_up
+            - src_card and dest_card have the SAME suit.
+            - src_card.rank() is 1 greater than dest_card.rank()
+                ~ src_card == 11, dest_card == 10, good.
+            - if src_card is None, de
+        """
+        # test face down
+        self.assertFalse(proj10.valid_fnd_move(self._face_down_card, self._heart_two_card),
+                msg="A face down src_card is not a valid move.")
+        self.assertFalse(proj10.valid_fnd_move(self._heart_two_card, self._face_down_card),
+                msg="A face down dest_card is not a valid move.")
+
+        # test diff suits
+        self.assertFalse(proj10.valid_fnd_move(self._heart_two_card, self._club_two_card),
+                msg="src_card msut have same suit as dest_card.")
+        # test low src_card suits
+        self.assertFalse(proj10.valid_fnd_move(self._heart_two_card, self._club_two_card),
+                msg="src_card must have rank 2 higher than dest_card.")
+
+        # test good input
+        self.assertTrue(proj10.valid_fnd_move(self._heart_three_card, self._heart_two_card),
+                msg="src_card with rank 1 higher than the dest_card and with same suit as dest_card is valid.")
+
+
 
 if __name__ == '__main__':
     unittest.main()
