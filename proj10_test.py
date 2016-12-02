d63db3b55408ed390bf2c85ae55e2eb700e122aa
@be <abeahmed2@gmail.com>
2016-12-02 01:18:27 -0500

    tab_to_fnd tests are done.

--- proj10_test.py
+++ proj10_test.py
@@ -29,6 +29,21 @@ class TestProj10(unittest.TestCase):
         - valid_tab_move
     """
 
+    """
+    Helper functions
+    """
+    def grab_test_tab_col(self, length=1):
+        """
+        Returns:
+           Tableau column default initialized with desired length.
+
+        Choose a length 1 - 7 inclusive
+        """
+        if not (1 <= length and length <= 7):
+            raise ValueError("1 - 7 tab length required.")
+
+        return self._tab[length - 1][:]
+
     def setUp(self):
         """
         Initialize a game.
@@ -65,6 +80,10 @@ class TestProj10(unittest.TestCase):
         """
         pass
 
+    """
+    Tests
+    """
+
     def testSetupGameFunctionTableau(self):
         """
         setup_game() -> tableau
@@ -79,7 +98,7 @@ class TestProj10(unittest.TestCase):
         # tableau
         self.assertListEqual(tab, self._tab,
                 msg='Tableau was not distributed in the correct manner. Check project PDF.\
-                        Are you sure your project is using cards1.py?')
+Are you sure your project is using cards1.py?')
 
     def testSetupGameFunctionStock(self):
         """
@@ -95,7 +114,7 @@ class TestProj10(unittest.TestCase):
         # stock
         self.assertListEqual(stock, self._stock,
                 msg='Stock was not setup in the correct manner.\nSize = {}. Check the project PDF.\
-                        Are you sure your project is using cards1.py?'.format(len(stock)))
+Are you sure your project is using cards1.py?'.format(len(stock)))
 
     def testSetupGameFunctionWaste(self):
         """
@@ -111,7 +130,7 @@ class TestProj10(unittest.TestCase):
         # waste
         self.assertListEqual(waste, self._waste,
                 msg='Waste was not setup in the correct manner.\nSize = {} It should have one card Check t eh project PDF.\
-                        Are you sure your project is using cards1.py?'.format(len(waste)))
+Are you sure your project is using cards1.py?'.format(len(waste)))
 
     # not used for project scoring
     '''
@@ -205,30 +224,175 @@ class TestProj10(unittest.TestCase):
         self.assertTrue(proj10.valid_fnd_move(self._heart_three_card, self._heart_two_card),
                 msg="dest_card with rank 1 higher than the src_card and with diff suit than dest_card is valid input.")
 
+# tableau_to_foundation empty test
 
-    def testTableauToFoundation(self):
+    def testTableauToFoundationEmptyPushBad(self):
         """
         tableau_to_foundation(tab_col : list, fnd_col : list)
         Returns:
             None
 
-        Pre-Conditions:
-
+        This test ensures pushing a card != ace to an empty fnd fails.
         """
-        # test a non-ace card to empty foundation
-        with self.assertRaises(RuntimeError):
+
+        # test empty foundation
+        with self.assertRaises(RuntimeError, msg="Empty foundation should throw error when passing a card that is not an ace."):
             # 2 of clubs
-            test_card = cards.Card(2, 1)
-            test_fnd = self._fnd[0][:]
-            test_tab = self._tab[0][:]
+            test_card = cards.Card(rank=2, suit=1)
+            test_tab = self.grab_test_tab_col(1)
             test_tab.append(test_card)
-            proj10.tableau_to_foundation(test_tab, test_fnd)
-        # with 
+            proj10.tableau_to_foundation(test_tab, [])
+
+    def testTableauToFoundationEmptyPushGood(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        This test ensures pushing an ace to an empty fnd succeeds.
+        """
+        # test adding ace to empty foundation
+        #
+        # ace of clubs
+        test_card = cards.Card(rank=1, suit=1)
+        test_tab = self.grab_test_tab_col(1)
+        test_tab.append(test_card)
+        test_fnd = []
+        proj10.tableau_to_foundation(test_tab, test_fnd)
+        self.assertEqual(test_card, test_fnd[-1],
+                msg="Pushing ace to an empty fnd should work fine.")
 
 
+# tableau_to_foundation rank test
+
+    def testTableauToFoundationRankBad(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Good suit, bad rank.
+        """
+        with self.assertRaises(RuntimeError, msg="Pushing a card to fnd requires it to the cards \
+rank to be 1 higher than what is on the top of the fnd."):
+            # test source tableau
+            test_tab_col_src = [cards.Card(rank=3, suit=1)]
+            # test destination foundation with an ace on top
+            test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+            proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+
+    def testTableauToFoundationRankGood(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Good suit, good rank.
+        """
+        # test source tableau
+        test_src_card = cards.Card(rank=2, suit=1)
+        test_tab_col_src = [test_src_card]
+        # test destination foundation with an ace on top
+        test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+        proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+        self.assertTrue(test_fnd_col_dest[-1] == test_src_card,
+                msg="tab_to_fnd should succed if tab[-1] is 1 rank higher than dest.")
+
+# tableau_to_foundation suit test
+
+    def testTableauToFoundationSuitGood(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Good suit, good rank.
+        """
+        # test source tableau
+        test_src_card = cards.Card(rank=2, suit=1)
+        test_tab_col_src = [test_src_card]
+        # test destination foundation with an ace on top
+        test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+        proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+        self.assertTrue(test_fnd_col_dest[-1] == test_src_card,
+                msg="tab_to_fnd should succed if tab[-1] is same suit as dest.")
 
 
+    def testTableauToFoundationSuitBad(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
 
+        Bad suit, good rank.
+        """
+        with self.assertRaises(RuntimeError, msg="Pushing a card to fnd requires it to the cards have same suit."):
+            # test source tableau
+            test_tab_col_src = [cards.Card(rank=2, suit=2)]
+            # test destination foundation with an ace on top
+            test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+            proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+
+
+# tableau_to_foundation face test
+
+    def testTableauToFoundationFaceGood(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Good suit, good rank, face up.
+        """
+        # test source tableau
+        test_src_card = cards.Card(rank=2, suit=1)
+        if not test_src_card.is_face_up():
+            test_src_card.flip_card()
+        test_tab_col_src = [test_src_card]
+        # test destination foundation with an ace on top
+        test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+        proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+        self.assertTrue(test_fnd_col_dest[-1] == test_src_card,
+                msg="tab_to_fnd should succed if tab[-1] is face up.")
+
+
+    def testTableauToFoundationFaceBad(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Bad suit, good rank.
+        """
+        with self.assertRaises(RuntimeError, msg="Pushing a card to fnd requires it to the cards have same suit."):
+            test_src_card = cards.Card(rank=2, suit=1)
+            if test_src_card.is_face_up():
+                test_src_card.flip_card()
+            # test source tableau
+            test_tab_col_src = [test_src_card]
+            # test destination foundation with an ace on top
+            test_fnd_col_dest = [cards.Card(rank=1, suit=1)]
+            proj10.tableau_to_foundation(test_tab_col_src, test_fnd_col_dest)
+
+
+# tableau_to_foundation last card in tableau face up
+
+    def testTableauToFoundationFlipLastTabCard(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Card at the bottom of the source tab_col should be flipped up.
+        """
+        test_tab_col = self.grab_test_tab_col(2)
+        # flip the last card and push something that is a valid move
+        test_tab_col[-1].flip_card()
+        test_tab_col.append(cards.Card(rank=2, suit=1))
+        test_fnd = [cards.Card(rank=1, suit=1)]
+        proj10.tableau_to_foundation(test_tab_col, test_fnd)
+        self.assertTrue(test_tab_col[-1].is_face_up(),
+                msg="Card at the bottom of a tableau cannot be face down after tab_to_fnd move.")
 
 if __name__ == '__main__':
     unittest.main()
