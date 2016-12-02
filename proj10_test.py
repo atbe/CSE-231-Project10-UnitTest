6b44295dce4d253e599a3781aba0e4c398835aaa
@be <abeahmed2@gmail.com>
2016-12-02 00:31:59 -0500

    Rethought how I did the setup_game tests based on the rubric. Default _tab, _stock, etc are all Enbody's values for the sake of correctness and predictability when testing. Began testing tablueaToFoundation, not yet done.

--- proj10_test.py
+++ proj10_test.py
@@ -15,22 +15,24 @@ class TestProj10(unittest.TestCase):
     cannot prevent the main loop of the students' code before
     running our tests.
 
-    An example to solutions for a problem like this can be found
+    An example to a solution for a problem like this can be found
     at the bottom of the proj10.py file provided as a sample
-    with this test.
+    with this test. It uses the global __name__ variable.
+    This way, we can import the module to use the functions like
+    we have for this test, and run the proj10.py alone for regular
+    output.
 
 
     Functions Tested:
         - setup_game
-        -
+        - valid_fnd_move
+        - valid_tab_move
     """
 
     def setUp(self):
         """
         Initialize a game.
         """
-        # grab a game
-        self._fnd, self._tab, self._stock, self._waste = proj10.setup_game()
         # grab a few cards
         self._heart_two_card = cards.Card(rank=2, suit=3)
         self._heart_three_card = cards.Card(rank=3, suit=3)
@@ -41,7 +43,6 @@ class TestProj10(unittest.TestCase):
         if self._face_down_card.is_face_up():
             self._face_down_card.flip_card()
 
-
         # with open('enbody_game_elements.pckl', 'wb') as outfile:
             # (fnd, tab, stock, waste) = proj10.setup_game()
             # enbody_game_elements = {'tab': tab, 'fnd':fnd, 'stock': stock, 'waste': waste}
@@ -50,7 +51,11 @@ class TestProj10(unittest.TestCase):
         self._enbody_game_elements_dict = {} # temp
         try:
             with open('enbody_game_elements.pckl', 'rb') as infile:
-                self._enbody_game_elements_dict = pickle.load(infile)
+                enbody_game_elements_dict = pickle.load(infile)
+                self._fnd = enbody_game_elements_dict['fnd']
+                self._tab = enbody_game_elements_dict['tab']
+                self._waste = enbody_game_elements_dict['waste']
+                self._stock = enbody_game_elements_dict['stock']
         except FileNotFoundError:
             raise FileNotFoundError('enbody_game_elements.pckl is required for this test.')
 
@@ -60,32 +65,86 @@ class TestProj10(unittest.TestCase):
         """
         pass
 
+    def testSetupGameFunctionTableau(self):
+        """
+        setup_game() -> tableau
+
+
+        Testing tableau from setup_game.
+        """
+
+        # grab a test game
+        fnd, tab, stock, waste = proj10.setup_game()
+
+        # tableau
+        self.assertListEqual(tab, self._tab,
+                msg='Tableau was not distributed in the correct manner. Check project PDF.\
+                        Are you sure your project is using cards1.py?')
+
+    def testSetupGameFunctionStock(self):
+        """
+        setup_game() -> stock
+
+
+        Testing stock from setup_game.
+        """
+
+        # grab a test game
+        fnd, tab, stock, waste = proj10.setup_game()
+
+        # stock
+        self.assertListEqual(stock, self._stock,
+                msg='Stock was not setup in the correct manner.\nSize = {}. Check the project PDF.\
+                        Are you sure your project is using cards1.py?'.format(len(stock)))
+
+    def testSetupGameFunctionWaste(self):
+        """
+        setup_game() -> waste
+
+
+        Testing waste from setup_game.
+        """
+
+        # grab a test game
+        fnd, tab, stock, waste = proj10.setup_game()
+
+        # waste
+        self.assertListEqual(waste, self._waste,
+                msg='Waste was not setup in the correct manner.\nSize = {} It should have one card Check t eh project PDF.\
+                        Are you sure your project is using cards1.py?'.format(len(waste)))
+
+    # not used for project scoring
+    '''
     def testSetupGameFunction(self):
         """
-        setup_game()
+        setup_game() -> tableau
 
 
         Tests the setup_game function by checking all of its return values
         against the ouput from Dr.Enbody's solution.
         """
 
+        # grab a test game
+        fnd, tab, stock, waste = proj10.setup_game()
+
         # Check all of the elements to ensure the game was setup correctly.
         #
         # foundation
-        self.assertListEqual(self._fnd, self._enbody_game_elements_dict['fnd'],
+        self.assertListEqual(fnd, self._fnd,
                 msg='Foundation should be a list containing 4 empty lists initially.')
         # tableau
-        self.assertListEqual(self._tab, self._enbody_game_elements_dict['tab'],
+        self.assertListEqual(tab, self._tab,
                 msg='Tableau was not distributed in the correct manner. Check project PDF.\
                         Are you sure your project is using cards1.py?')
         # stock
-        self.assertListEqual(self._stock, self._enbody_game_elements_dict['stock'],
+        self.assertListEqual(stock, self._stock,
                 msg='Stock was not setup in the correct manner. Check the project PDF.\
                         Are you sure your project is using cards1.py?')
         # waste
-        self.assertListEqual(self._waste, self._enbody_game_elements_dict['waste'],
-                msg='Waste was not setup in the correct manner. Check t eh project PDF.\
+        self.assertListEqual(waste, self._waste,
+                msg='Waste was not setup in the correct manner. It should have one card Check t eh project PDF.\
                         Are you sure your project is using cards1.py?')
+    '''
 
     def testValidFoundationMoveFunction(self):
         """
@@ -145,6 +204,28 @@ class TestProj10(unittest.TestCase):
         # test good input
         self.assertTrue(proj10.valid_fnd_move(self._heart_three_card, self._heart_two_card),
                 msg="dest_card with rank 1 higher than the src_card and with diff suit than dest_card is valid input.")
+
+
+    def testTableauToFoundation(self):
+        """
+        tableau_to_foundation(tab_col : list, fnd_col : list)
+        Returns:
+            None
+
+        Pre-Conditions:
+
+        """
+        # test a non-ace card to empty foundation
+        with self.assertRaises(RuntimeError):
+            # 2 of clubs
+            test_card = cards.Card(2, 1)
+            test_fnd = self._fnd[0][:]
+            test_tab = self._tab[0][:]
+            test_tab.append(test_card)
+            proj10.tableau_to_foundation(test_tab, test_fnd)
+        # with 
+
+
 
 
 
